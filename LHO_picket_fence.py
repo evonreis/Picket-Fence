13450220633070e0283e9d14519812a0f2395159
IAguilar007 <81662195+IAguilar007@users.noreply.github.com>
2022-11-04 01:42:23 -0700

    Update LHO_picket_fence.py

--- LHO_picket_fence.py
+++ LHO_picket_fence.py
@@ -482,7 +482,7 @@ def main():
              'in "NETWORK"_"STATION" format and "selector" a space separated '
              'list of "LOCATION""CHANNEL", e.g. '
              '"IU_KONO:BHE BHN,MN_AQU:HH?.D".', 
-             default="US_HLID:00BHZ, US_NEW:00BHZ, US_NLWA:00BHZ, CN_SHB:HHZ, IU_COR:00BHZ, US_MSO:00BHZ")
+             default="US_HLID:00BHZ, US_NEW:00BHZ, US_NLWA:00BHZ, CN_VDEB:HHZ, UO_FHAC:HHZ, US_MSO:00BHZ")
     # Real-time parameters
     parser.add_argument(
         '--seedlink_server', type=str,
