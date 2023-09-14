76803f44ac717666f13da7abebeb94faadd7bd42
Edgard Bonilla <eddbonilla@Edgards-MacBook-Pro.local>
2023-09-14 16:58:20 -0700

    Eliminated the dependency of LLO-Server.py on gwpy. the two server scripts are now in sync

--- LLO-Server.py
+++ LLO-Server.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 from pcaspy import Driver, SimpleServer
-from gwpy.time import tconvert
 
 class myDriver(Driver):
     def  __init__(self):
@@ -62,6 +61,8 @@ def Reverse_ID(n):
         result += chr(int(s[i:i+2], 16))
     return result
 
+
+
 def main():
     server = SimpleServer()
 
@@ -80,8 +81,8 @@ def main():
     driver.setParam("NETWORK_AUX2", -1)
     driver.setParam("NETWORK_AUX3", -1)
     driver.setParam("NETWORK_STATION_NAME", "")
-    driver.setParam("SERVER_START_GPS", tconvert("now").seconds)
-    driver.setParam("SERVER_GPS", tconvert("now").seconds)
+    driver.setParam("SERVER_START_GPS", 0)
+    driver.setParam("SERVER_GPS", 0)
     
     # process CA transactions
     while True:
